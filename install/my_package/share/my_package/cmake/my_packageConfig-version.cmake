3d57f25613d5f396c059223464b237b8412d7b1c
Aleks Santari <aleksantari@gmail.com>
2025-04-11 20:59:29 +0000

    initial commit

---
+++ install/my_package/share/my_package/cmake/my_packageConfig-version.cmake
@@ -0,0 +1,1 @@
+/home/aleksantari/ros2_robosub/build/my_package/ament_cmake_core/my_packageConfig-version.cmake
---
+++ install/my_package/share/my_package/local_setup.bash
@@ -0,0 +1,1 @@
+/home/aleksantari/ros2_robosub/build/my_package/ament_cmake_environment_hooks/local_setup.bash
